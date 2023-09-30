236d08ed529005c669d70ee08aa84e64f2fe9e1e
Cynthia Zapata <cynthiazapata2017@gmail.com>
2023-09-29 20:35:53 -0500

    added style sheets for app

--- live-chat/pages/index.js
+++ live-chat/pages/index.js
@@ -6,6 +6,6 @@ import styles from '@/styles/Home.module.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return <div>Hellow world!</div>
+  return <div>Hello world!</div>
    
 }
